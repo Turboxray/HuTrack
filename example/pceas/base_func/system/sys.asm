843daa8414aeea7729afff8823927ed96a8f4460
Rick <leverton23@msn.com>
2023-01-15 14:20:18 -0700

    Clean up tabs

--- example/pceas/base_func/system/sys.asm
+++ example/pceas/base_func/system/sys.asm
@@ -4,13 +4,13 @@
 
 ;//enables soft reset
 enable_pad_reset:
-			lda #$AA
-			sta soft_reset+0
-			lda	#$55
-			sta soft_reset+1
-			lda #$3c
-			sta soft_reset+2
-		rts
+            lda #$AA
+            sta soft_reset+0
+            lda #$55
+            sta soft_reset+1
+            lda #$3c
+            sta soft_reset+2
+        rts
 ;#end
 
 

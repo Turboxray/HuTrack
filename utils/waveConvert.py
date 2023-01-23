e981138602feab1409c4beba1f63e9387784b881
Turboxray <turboxraypce@gmail.com>
2023-01-22 19:06:21 -0800

    update

--- utils/waveConvert.py
+++ utils/waveConvert.py
@@ -81,165 +81,297 @@ class ConvertWave():
 
         sampleObj.samplePCE = newSampleData[::]
 
-# ///////////////////////////////////////////////////////////////////////
-# int get_file_info( FILE *bin )
-# {
+
+class WavRead():
+
+    def __init__(self, filename, debug=False):
+        self.wavHeader      = {}
+        self.wavContents    = []
+        self.contentIndex   = 0
+        self.content        = None
+        self.filename       = filename
+        self.message        = ''
+        self.debug          = debug
+
+    def readFile(self):
+        try:
+            with open(self.filename,'rb') as f:
+                self.content = f.read()
+                self.content = [int(item) & 0xff for item in self.content]
+        except Exception as e:
+            print(f'Error reading contents: {e}')
+            return False
+
+        print('File read.')
+
+        result = self.content != None and self.getWavHeader() and self.getWaveData()
+        
+        return result, self.message, self.wavHeader, self.wavContents
 
 
-#             rewind(bin);
-#             RiffID=int_read_be(bin);
-#             RiffIDsize=int_read_le(bin);
-#             WaveID=int_read_be(bin);
-#             FMT=int_read_be(bin);
-#             WaveIDsize=int_read_le(bin);
+    # ///////////////////////////////////////////////////////////////////////
+    def getWavHeader(self):
 
-#             FormatTag=wrd_read_le(bin);
-#             Channels=wrd_read_le(bin);
+        RiffID          = self.read4cc()
+        RiffIDsize      = self.intReadLE()
+        WaveID          = self.read4cc()
 
-#             SamplesPerSec=int_read_le(bin);
-#             AvgBytesPerSec=int_read_le(bin);
+        FmtID           = self.read4cc()
+        FMTsize         = self.intReadLE()
+        fmtType         = self.wordReadLE()
 
-#             BlockAlign=wrd_read_le(bin);
-#             BitsPerSample=wrd_read_le(bin);
+        Channels        = self.wordReadLE()
 
-#             DataID=int_read_be(bin);
-#             DataIDsize=int_read_le(bin);
+        SamplesPerSec   = self.intReadLE()
+        AvgBytesPerSec  = self.intReadLE()
+
+        BlockAlign      = self.wordReadLE()
+        BitsPerSample   = self.wordReadLE()
+
+        DataID          = self.read4cc()
+        DataIDsize      = self.intReadLE()
+
+        if RiffID != 'RIFF':
+            self.message = "\n Possible incorrect or corrupt wave file. No RIFF ID. \n"
+        elif WaveID != 'WAVE':
+            self.message = "\n Possible incorrect or corrupt wave file. No WAVE ID. \n"
+        elif FmtID != 'fmt ':
+            self.message = "\n Possible incorrect or corrupt wave file. No FMT ID. \n"
+        elif DataID != 'data':
+            self.message = "\n Possible incorrect or corrupt wave file. No DATA ID. \n"
+        elif FMTsize != 0x10:
+            self.message = "\n Unsupported RIFF FMT format or correct wavefile. \n"
+        elif (BitsPerSample!=8) and (BitsPerSample!=16):
+            self.message = f"\n Wave file must be 8bit or 16bit in depth. Found: {BitsPerSample}bit\n"
+        elif Channels > 1:
+            self.message = f"\n More than 1 channel support isn't currently implemented. \n"
+        else:
+            self.wavHeader = {
+                'RiffID'            : RiffID,
+                'RiffIDsize'        : RiffIDsize,
+                'WaveID'            : WaveID,
+                'FMT'               : FmtID,
+                'FMTsize'           : FMTsize,
+                'fmtType'           : fmtType,
+                'Channels'          : Channels,
+                'SamplesPerSec'     : SamplesPerSec,
+                'AvgBytesPerSec'    : AvgBytesPerSec,
+                'BlockAlign'        : BlockAlign,
+                'BitsPerSample'     : BitsPerSample,
+                'DataID'            : DataID,
+                'DataIDsize'        : DataIDsize
+            }
+
+            self.message  = f" Bit depth: {BitsPerSample}\n"
+            self.message += f" Channels:  {Channels}\n"
+            self.message += f" Rate:      {SamplesPerSec}\n"
+        
+        print(self.message)
+
+        return self.wavHeader != {}
+
+    # ///////////////////////////////////////////////////////////////////////
+    def getWaveData(self):
+
+        numChans    = self.wavHeader['Channels']
+        pcmSize     = self.wavHeader['BitsPerSample'] // 8
+        step        = numChans * pcmSize
+
+        print(f' {self.contentIndex}, {len(self.content)}, {step}, {pcmSize}')
+
+        for idx in range(self.contentIndex, len(self.content), step):
+
+            vals = self.content[idx:idx+step]
+
+            for vidx in range(0,len(vals),pcmSize):
+                sample = 0
+                sample  |= int(vals[vidx]) & 0xff
+                if pcmSize == 2:
+                    sample |= (int(vals[vidx+1]) & 0xff) << 8
+                self.wavContents.append(sample)
+
+        if self.debug:
+            with open('debug.bin','wb') as f:
+                for val in self.wavContents:
+                    if pcmSize == 2:
+                        f.write(bytearray([val>>8 & 0xff]))
+                    f.write(bytearray([val & 0xff]))
+
+        return self.wavContents != []
+
+    # ///////////////////////////////////////////////////////////////////////
+    # Helper functions
+    # ///////////////////////////////////////////////////////////////////////
+
+    def read4cc(self):
+        data = self.content[self.contentIndex : self.contentIndex+4 ]
+        self.contentIndex += 4
+        return ''.join([chr(val) for val in data])
+    
+    def intReadBE(self):
+        idx = self.contentIndex
+        self.contentIndex += 4
+        val  = self.content[idx+0:idx+1][0] << 24
+        val |= self.content[idx+1:idx+2][0] << 16
+        val |= self.content[idx+2:idx+3][0] <<  8
+        val |= self.content[idx+3:idx+4][0] <<  0
+        return val
+
+    def intReadLE(self):
+        idx = self.contentIndex
+        self.contentIndex += 4
+        # print(f'debug data: {self.content[idx+0:idx+1]}')
+        val  = self.content[idx+0:idx+1][0] <<  0
+        val |= self.content[idx+1:idx+2][0] <<  8
+        val |= self.content[idx+2:idx+3][0] << 16
+        val |= self.content[idx+3:idx+4][0] << 24
+        return val
+
+    def wordReadBE(self):
+        idx = self.contentIndex
+        self.contentIndex += 2
+        val  = self.content[idx+0:idx+1][0] << 8
+        val |= self.content[idx+1:idx+2][0] << 0
+        return val
+
+    def wordReadLE(self):
+        idx = self.contentIndex
+        self.contentIndex += 2
+        val  = self.content[idx+0:idx+1][0] << 0
+        val |= self.content[idx+1:idx+2][0] << 8
+        return val
+
+class GuiFrontend():
+
+    def __init__(self):
+        pcmData         = []
+        pcmHeader       = {}
+        self.components = {}
+
+    def openWave(self):
+
+        global hutrack, components
+
+        filename = fd.askopenfilename(defaultextension='.wav', filetypes = (("wav files","*.wav"),("all files","*")))
+        filename = filename
+
+        if filename == '' or filename == None:
+            print('Cancel open..')
+            return
+        
+        result, convertInfo, pcmHeader, pcmData = WavRead(filename).readFile()
+
+        if not result and tk.messagebox.showerror(title=None, message=convertInfo):
+            return
+        
+        tk.messagebox.showinfo(title=None, message=convertInfo)
+
+        return result
 
 
-#             if(RiffID!='RIFF')
-#               { printf("\n Possible incorrect or corrupt wave file. No RIFF ID. \n"); fclose(bin); exit(-1); }
-#             if(WaveID!='WAVE')
-#               { printf("\n Possible incorrect or corrupt wave file. No WAVE ID. \n"); fclose(bin); exit(-1); }
-#             if(FMT!='fmt ')
-#               { printf("\n Possible incorrect or corrupt wave file. No FMT ID. \n"); fclose(bin); exit(-1); }
-#             if(DataID!='data')
-#               { printf("\n Possible incorrect or corrupt wave file. No DATA ID. \n"); fclose(bin); exit(-1); }
-#             if((BitsPerSample!=8) && (BitsPerSample!=16))
-#               { printf("\n Wave file must be 8bit or 16bit in depth. \n"); fclose(bin); exit(-1); }
 
-#             printf("\n Bit depth: %d", BitsPerSample);
-#             printf("\n Channels: %d", Channels);
-#             printf("\n Rate: %dhz", SamplesPerSec);
+    def saveFile(self):
+        tk.messagebox.showinfo(title=None, message='HuTrack pce file saved.')
 
 
-
-#       return 0;
-# }
-
-def openWave():
-
-    global hutrack, components
-
-    filename = fd.askopenfilename(defaultextension='.dmf', filetypes = (("dmf files","*.dmf"),("all files","*")))
-    filename = filename
-
-    if filename == '' or filename == None:
-        print('Cancel open..')
-        return
+    def componentState(self, compState):
+            for child in self.components["subframe1"].winfo_children():
+                child.configure(state=compState)
+            for child in self.components["subframe2"].winfo_children():
+                child.configure(state=compState)
+            self.components["save"].config(state=compState)
 
 
-def saveFile():
-    tk.messagebox.showinfo(title=None, message='HuTrack pce file saved.')
+    def process(self):
+        root = tk.Tk()
+        self.components['root'] = root
+        root.title("Wave to HuTrack PCM Converter")
 
 
-def componentState(compState):
-        for child in components["subframe1"].winfo_children():
-            child.configure(state=compState)
-        for child in components["subframe2"].winfo_children():
-            child.configure(state=compState)
-        components["export"].config(state=compState)
-        components["save"].config(state=compState)
+        frame1 = tk.LabelFrame(root,padx=24, pady=24)
+        frame1.pack()
 
-def initGui():
-    root = tk.Tk()
-    components['root'] = root
-    root.title("DMF to HuTrack Converter")
+        subframe1 = tk.LabelFrame(frame1, padx=4, pady=4)
+        subframe1.pack(side=tk.LEFT)
+        self.components['subframe1'] = subframe1
 
+        labelTop = ttk.Label(subframe1, text = "Filter Type")
+        labelTop.grid(column=0, row=0)
+        filterCombo = ttk.Combobox(subframe1, values=filterValues)
+        self.components['filter'] = filterCombo
+        filterCombo.grid(column=0, row=1)
+        filterCombo.current(0)
 
-    frame1 = tk.LabelFrame(root,padx=24, pady=24)
-    frame1.pack()
+        labelTop = ttk.Label(subframe1, text = "Bit Depth")
+        labelTop.grid(column=0, row=2)
+        bitdepthCombo = ttk.Combobox(subframe1, values=bitdepthValues)
+        self.components['bitdepth'] = bitdepthCombo
+        bitdepthCombo.grid(column=0, row=3)
+        bitdepthCombo.current(2)
 
-    subframe1 = tk.LabelFrame(frame1, padx=4, pady=4)
-    subframe1.pack(side=tk.LEFT)
-    components['subframe1'] = subframe1
+        labelTop = ttk.Label(subframe1, text = "Playback Rate")
+        labelTop.grid(column=0, row=4)
+        playerateCombo = ttk.Combobox(subframe1, values=playerateValues)
+        self.components['playback'] = playerateCombo
+        playerateCombo.grid(column=0, row=5)
+        playerateCombo.current(0)
 
-    labelTop = ttk.Label(subframe1, text = "Filter Type")
-    labelTop.grid(column=0, row=0)
-    filterCombo = ttk.Combobox(subframe1, values=filterValues)
-    components['filter'] = filterCombo
-    filterCombo.grid(column=0, row=1)
-    filterCombo.current(0)
+        labelTop = ttk.Label(subframe1, text = "Debug")
+        labelTop.grid(column=0, row=6)
+        debugCombo = ttk.Combobox(subframe1, values=['Off','On'])
+        self.components['debug'] = debugCombo
+        debugCombo.grid(column=0, row=7)
+        debugCombo.current(0)
 
-    labelTop = ttk.Label(subframe1, text = "Bit Depth")
-    labelTop.grid(column=0, row=2)
-    bitdepthCombo = ttk.Combobox(subframe1, values=bitdepthValues)
-    components['bitdepth'] = bitdepthCombo
-    bitdepthCombo.grid(column=0, row=3)
-    bitdepthCombo.current(2)
-
-    labelTop = ttk.Label(subframe1, text = "Playback Rate")
-    labelTop.grid(column=0, row=4)
-    playerateCombo = ttk.Combobox(subframe1, values=playerateValues)
-    components['playback'] = playerateCombo
-    playerateCombo.grid(column=0, row=5)
-    playerateCombo.current(0)
-
-    labelTop = ttk.Label(subframe1, text = "Debug")
-    labelTop.grid(column=0, row=6)
-    debugCombo = ttk.Combobox(subframe1, values=['Off','On'])
-    components['debug'] = debugCombo
-    debugCombo.grid(column=0, row=7)
-    debugCombo.current(0)
-
-    labelTop = ttk.Label(subframe1, text = "Bit Pack PCM")
-    labelTop.grid(column=0, row=8)
-    bitpackCombo = ttk.Combobox(subframe1, values=['Off','On'])
-    components['bitpack'] = bitpackCombo
-    bitpackCombo.grid(column=0, row=9)
-    bitpackCombo.current(0)
+        labelTop = ttk.Label(subframe1, text = "Bit Pack PCM")
+        labelTop.grid(column=0, row=8)
+        bitpackCombo = ttk.Combobox(subframe1, values=['Off','On'])
+        self.components['bitpack'] = bitpackCombo
+        bitpackCombo.grid(column=0, row=9)
+        bitpackCombo.current(0)
 
 
 
-    subframe2 = tk.LabelFrame(frame1, padx=4, pady=4)
-    subframe2.pack(side=tk.LEFT)
-    components['subframe2'] = subframe2
+        subframe2 = tk.LabelFrame(frame1, padx=4, pady=4)
+        subframe2.pack(side=tk.LEFT)
+        self.components['subframe2'] = subframe2
 
-    labelTop = ttk.Label(subframe2, text = "Include Path: ")
-    labelTop.grid(column=0, row=0)
-    includePath = tk.Entry(subframe2)
-    components['path'] = includePath
-    includePath.grid(column=1, row=0)
-    labelTop = ttk.Label(subframe2, text = "Song name: ")
-    labelTop.grid(column=0, row=1)
-    songname = tk.Entry(subframe2)
-    components['song'] = songname
-    songname.grid(column=1, row=1)
-    labelTop = ttk.Label(subframe2, text = "dest path: ")
-    labelTop.grid(column=0, row=2)
-    destpath = tk.Entry(subframe2)
-    components['destpath'] = destpath
-    destpath.grid(column=1, row=2)
-    labelTop = ttk.Label(subframe2, text = "subFolder: ")
-    labelTop.grid(column=0, row=2)
-    subFolder = tk.Entry(subframe2)
-    components['subFolder'] = subFolder
-    subFolder.grid(column=1, row=2)
-
-
-    subframe3 = tk.LabelFrame(frame1, padx=4, pady=4)
-    subframe3.pack(side=tk.LEFT)
-    components['subframe3'] = subframe3
-
-    openButton   = tk.Button(subframe3, text='     Open DMF       ', command=openWave)
-    openButton.grid(row=0, column=0,sticky=tk.W)
-    saveButton   = tk.Button(subframe3, text='     Save DMF    ', command=saveFile)
-    components['save'] = saveButton
-    saveButton.grid(row=2, column=0,sticky=tk.W)
+        labelTop = ttk.Label(subframe2, text = "Include Path: ")
+        labelTop.grid(column=0, row=0)
+        includePath = tk.Entry(subframe2)
+        self.components['path'] = includePath
+        includePath.grid(column=1, row=0)
+        labelTop = ttk.Label(subframe2, text = "PCM name: ")
+        labelTop.grid(column=0, row=1)
+        songname = tk.Entry(subframe2)
+        self.components['song'] = songname
+        songname.grid(column=1, row=1)
+        labelTop = ttk.Label(subframe2, text = "dest path: ")
+        labelTop.grid(column=0, row=2)
+        destpath = tk.Entry(subframe2)
+        self.components['destpath'] = destpath
+        destpath.grid(column=1, row=2)
+        labelTop = ttk.Label(subframe2, text = "subFolder: ")
+        labelTop.grid(column=0, row=2)
+        subFolder = tk.Entry(subframe2)
+        self.components['subFolder'] = subFolder
+        subFolder.grid(column=1, row=2)
 
 
-    componentState(tk.DISABLED)
+        subframe3 = tk.LabelFrame(frame1, padx=4, pady=4)
+        subframe3.pack(side=tk.LEFT)
+        self.components['subframe3'] = subframe3
 
-    root.mainloop()
+        openButton   = tk.Button(subframe3, text='     Open WAV       ', command=self.openWave)
+        openButton.grid(row=0, column=0,sticky=tk.W)
+        saveButton   = tk.Button(subframe3, text='     Save HuPCM    ', command=self.saveFile)
+        self.components['save'] = saveButton
+        saveButton.grid(row=2, column=0,sticky=tk.W)
+
+
+        self.componentState(tk.DISABLED)
+
+        root.mainloop()
 
 
 #############################################################################################################
@@ -314,4 +446,4 @@ runOptions['alignPCM256']     = args.alignPCM256
 runOptions['debug']           = args.debug
 
 
-initGui()
+GuiFrontend().process()

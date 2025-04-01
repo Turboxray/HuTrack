90765fe846186042ed83ccf71d607e472f108f0c
Turboxray <turboxraypce@gmail.com>
2025-04-01 09:00:37 -0700

    init

--- example/hucc/main/build_hucc_S-RECORD.bat
+++ example/hucc/main/build_hucc_S-RECORD.bat
@@ -8,14 +8,16 @@ pushd
 
 del C_log.txt
 del S_log.txt
+del *.sym
+del *.lst
 del *.s
 
-set PCE_INCLUDE=%CD%\..\..\..\lib;C:\huc\include\huc
+set PCE_INCLUDE=%CD%\..\..\..\lib;C:\huc\include\hucc
 set PATH=%PATH%;c:\huc\bin
 
-huc.exe  -v -v -msmall -fno-recursive -s huc_example.c -T > C_log.txt
+hucc.exe -s -v -v -msmall -gL -fno-recursive -s hucc_example.c > C_log.txt
 
-pceas.exe huc_example.s -l 3 -S -m > S_log.txt
+pceas.exe --hucc hucc_example.s -l 3 -S -m > S_log.txt
 type C_log.txt
 type S_log.txt
 pause

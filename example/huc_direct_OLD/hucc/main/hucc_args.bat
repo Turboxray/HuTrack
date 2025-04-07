7ca654888d19f2dbc5686e32cf9639c859c632da
Turboxray <turboxraypce@gmail.com>
2025-04-06 21:06:32 -0700

    new changes in examples

--- example/huc_direct_OLD/hucc/main/hucc_args.bat
+++
@@ -1,9 +0,0 @@
-rem ***************************************************************************
-
-setlocal
-
-set PCE_INCLUDE=%CD%\..\..\..\lib;C:\huc\include\hucc
-set PATH=%PATH%;c:\huc\bin
-
-hucc.exe  
-pause
